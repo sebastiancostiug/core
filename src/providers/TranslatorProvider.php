31e42c3a31513a6a217f8ece8f1ac91f2d486691
Sebastian Costiug <sebastian@my-frontline.net>
2024-02-01 12:47:21 +0200

    translation

---
+++ src/providers/TranslatorProvider.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ *
+ * @package     slim-api-skeleton
+ *
+ * @subpackage  TranslatorProvider
+ *
+ * @author      Sebastian Costiug <sebastian@overbyte.dev>
+ * @copyright   2019-2024 Sebastian Costiug
+ * @license     https://opensource.org/licenses/BSD-3-Clause
+ *
+ * @category    slim-api-skeleton
+ * @see
+ *
+ * @since       2024-02-01
+ *
+ */
+
+namespace core\providers;
+
+use core\components\ServiceProvider;
+
+/**
+ * TranslatorProvider class
+ */
+class TranslatorProvider extends ServiceProvider
+{
+    /**
+     * register()
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * boot()
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //  placeholder
+    }
+}

3d2f1bce20f793edcbd87d235e3f1d843e8e2ef6
Sebastian Costiug <sebastian@my-frontline.net>
2024-02-02 11:57:53 +0200

    validator reworked

--- src/components/Validator.php
+++ src/components/Validator.php
@@ -18,24 +18,103 @@
 
 namespace core\components;
 
+use common\Translator;
+
 /**
  * Validator class
  */
 class Validator
 {
     /**
+     * @var array $data Holds the data to be validated.
+     */
+    protected $data;
+
+    /**
      * @var array $errors Holds the validation errors.
      */
     protected $errors;
 
     /**
+     * The Translator implementation.
+     *
+     * @var Translator
+     */
+    protected $translator;
+
+    /**
+     * All of the custom validator message replacers.
+     *
+     * @var array<string, \Closure|string>
+     */
+    protected $replacers = [];
+
+    /**
+     * All of the fallback messages for custom rules.
+     *
+     * @var array<string, string>
+     */
+    protected $fallbackMessages = [];
+
+    /**
+     * The Validator resolver instance.
+     *
+     * @var \Closure
+     */
+    protected $resolver;
+
+    /**
+     * @var array $rules Holds the validation rules.
+     */
+    protected $rules;
+
+    /**
+     * All of the custom validator extensions.
+     *
+     * @var array<string, \Closure|string>
+     */
+    protected $extendedRules;
+
+    /**
+     * @var array $filters Holds the filters to be applied.
+     */
+    protected $filters;
+
+    /**
+     * @var array $extnededFilters Holds the extended filters to be applied.
+     */
+    protected $extendedFilters;
+
+    /**
      * Constructor for the Validator class.
      *
-     * @param array $errors An array of errors.
+     * @param Translator $translator The Translator implementation.
+     * @param array      $data       The data to be validated.
+     *
+     * @return void
      */
-    public function __construct(array $errors)
+    public function __construct(Translator $translator, array $data)
     {
-        $this->errors = $errors;
+        $this->translator = $translator;
+        $this->data = $data;
+
+        $this->rules = [
+        'required'  => fn ($field) => !isset($this->data[$field]) || $this->data[$field] === '',
+        'email'     => fn ($field) => !filter_var($this->data[$field], FILTER_VALIDATE_EMAIL),
+        'strength'  => fn ($field) => !preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*(_|[^\w])).+$/', $this->data[$field] ?? ''),
+        'lengthMin' => fn ($field, $condition) => strlen($this->data[$field]) < $condition,
+        'lengthMax' => fn ($field, $condition) => strlen($this->data[$field]) > $condition,
+        'match'     => fn ($field, $condition) => $this->data[$field] !== $this->data[$condition],
+        'in'        => fn ($field, $condition) => !in_array($this->data[$field], explode(',', $condition)),
+        'unique'    => fn ($field, $condition) => (new $condition())->exists($field, $this->data[$field])
+        ];
+
+        $this->filters = [
+        'trim'      => fn ($field) => trim($this->data[$field]),
+        'stripTags' => fn ($field) => strip_tags($this->data[$field]),
+        'lowercase' => fn ($field) => strtolower($this->data[$field]),
+        'hash'      => fn ($field) => password_hash($this->data[$field], PASSWORD_DEFAULT)
+        ];
     }
 
     /**
@@ -48,13 +127,159 @@ class Validator
         return !empty($this->errors);
     }
 
-    /**
-     * Get the errors from the validator.
-     *
-     * @return array The array of errors.
-     */
+        /**
+         * Get the errors from the validator.
+         *
+         * @return array The array of errors.
+         */
     public function errors()
     {
         return $this->errors;
+    }
+
+        /**
+     * Enforces validation rules on the given data using the specified rules and messages.
+     *
+     * @param array $rules    The validation rules to be applied.
+     * @param array $messages The custom error messages to be used.
+     *
+     * @return $this The current instance of the Validator.
+     */
+    public function enforce(array $rules, array $messages = []): Validator
+    {
+        foreach ($rules as $field => $fieldRules) {
+            $fieldRules = explode('|', $fieldRules);
+
+            foreach ($fieldRules as $rule) {
+                $condition = null;
+                if (strpos($rule, ':') !== false) {
+                    list($rule, $condition) = explode(':', $rule);
+                }
+
+                $message = $messages[$field][$rule] ?? $this->translator->translate("validation.{$rule}", ['attribute' => $field]);
+
+                switch ($rule) {
+                    case 'required':
+                    case 'email':
+                    case 'strength':
+                        if (!$this->rules[$rule]($field)) {
+                            $this->errors[$field] = $message;
+                        }
+                        break;
+
+                    case 'lengthMin':
+                    case 'lengthMax':
+                    case 'match':
+                    case 'in':
+                    case 'unique':
+                        if (!$this->rules[$rule]($field, $condition)) {
+                            $this->errors[$field] = $message;
+                        }
+                        break;
+
+                    default:
+                        if (isset($this->extendedRules[$rule])) {
+                            $this->validate($rule, $field, $condition);
+                        }
+                        break;
+                }
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Filters the given data based on the provided filters.
+     *
+     * @param array $filters The filters to be applied.
+     *
+     * @return $this The current instance of the Validator.
+     */
+    public function filter(array $filters): Validator
+    {
+        foreach ($filters as $field => $fieldFilters) {
+            $fieldFilters = explode('|', $fieldFilters);
+
+            foreach ($fieldFilters as $filter) {
+                if (strpos($filter, ':') !== false) {
+                    $filter    = explode(':', $filter);
+                    $filter    = $filter[0];
+                    $condition = $filter[1];
+                }
+                switch ($filter) {
+                    case 'trim':
+                    case 'stripTags':
+                    case 'lowercase':
+                    case 'hash':
+                        $this->data[$field] = $this->filters[$filter]($field);
+                        break;
+
+                    default:
+                        if (isset($this->extendedFilters[$filter])) {
+                            $this->filter([$filter]);
+                        }
+                        break;
+                }
+            }
+        }
+        return $this;
+    }
+
+    /**
+     * Adds a custom validator extension.
+     *
+     * @param string          $rule      The name of the rule.
+     * @param \Closure|string $extension The extension to be added.
+     *
+     * @return void
+     */
+    public function rule($rule, $extension)
+    {
+        $this->extendedRules[$rule] = $extension;
+    }
+
+    /**
+     * Calls a custom validator extension.
+     *
+     * @param string $rule      The name of the rule.
+     * @param string $field     The name of the field to be validated.
+     * @param string $condition The condition to be applied.
+     *
+     * @return void
+     */
+    protected function validate($rule, $field, $condition)
+    {
+        $extension = $this->extendedRules[$rule];
+
+        if ($extension instanceof \Closure) {
+            $extension($rule, $this->data, $field, $condition);
+        } elseif (is_string($extension)) {
+            $this->resolve($extension)->validate($rule, $this->data, $field, $condition);
+        }
+    }
+
+    /**
+     * Resolves a validator extension.
+     *
+     * @param string $extension The extension to be resolved.
+     *
+     * @return mixed The resolved extension.
+     */
+    protected function resolve($extension)
+    {
+        return call_user_func($this->resolver, $extension);
+    }
+
+    /**
+     * Sets the Validator resolver instance.
+     *
+     * @param \Closure $resolver The Validator resolver instance.
+     *
+     * @return void
+     */
+    public function setResolver(\Closure $resolver)
+    {
+        $this->resolver = $resolver;
     }
 }
